78014c7c2fb4cffa69f0f75263a7a898e9e21642
Aaroncu9 <liuchuanlong9@163.com>
2023-07-02 22:40:35 +0800

    gin redireft

--- main.go
+++ main.go
@@ -7,31 +7,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type user struct {
-	UserName string `form:"username"`
-	Password string `form:"password"`
-}
-
 func main() {
 	// 默认路由
 	rt := gin.Default()
 
 	// 指定get 请求 json路径，调用指定函数
-	rt.GET("/user", func(ctx *gin.Context) {
-		// get request
-		var u user
-		err := ctx.ShouldBind(&u)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{
-				"err": err.Error(),
-			})
-		} else {
-			fmt.Printf("user info %#v\n", u)
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "ok",
-			})
-		}
+	rt.GET("/index", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusMovedPermanently, "http://www.baidu.com")
+	})
 
+	rt.GET("/a", func(ctx *gin.Context) {
+		ctx.Request.URL.Path = "/b"
+		rt.HandleContext(ctx)
+	})
+
+	rt.GET("/b", func(ctx *gin.Context) {
+		fmt.Println("hello i am in b")
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": ctx.Request.URL,
+		})
 	})
 
 	// 默认0.0.0.1：8080 run
