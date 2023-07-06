ac0e8b88c8566451ef5f3041994eccf98340eab5
Aaron <liuchuanlong9@163.com>
2023-07-06 13:50:53 +0800

    shouldbindjson commit

--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+// curl http://127.0.0.1:8080/loginJSON -H 'content-type:application/json' -d {"user":"root","password":"root"} -X POST
+// curl http://127.0.0.1:8080/loginJSON -H 'content-type:application/json' -d {\"user\":\"root\",\"password\":\"root\"} -X POST
+// curl http://127.0.0.1:8080/loginJSON -H 'content-type:application/json' -d {\"user\":\\"root\",\"password\":\"root2\"} -X POST
+
+// 定义接收数据的结构体
+type Login struct {
+	// binding:"required"修饰的字段，若接收为空值，则报错，是必须字段
+	User    string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
+	Pssword string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
+}
+
 func main() {
-	// 默认路由
-	rt := SetupRouter()
+	// 1.创建路由
+	// 默认使用了2个中间件Logger(), Recovery()
+	r := gin.Default()
+	// JSON绑定
+	r.POST("/loginJSON", func(c *gin.Context) {
+		// 声明接收的变量
+		var json Login
+		// 将request的body中的数据，自动按照json格式解析到结构体
+		if err := c.ShouldBindJSON(&json); err != nil {
+			// 返回错误信息
+			// gin.H封装了生成json数据的工具
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		// 判断用户名密码是否正确
+		if json.User != "root" || json.Pssword != "root" {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "200"})
+	})
 
-	// 默认0.0.0.1：8080 run
-	if err := rt.Run(); err != nil {
-		fmt.Printf("startup service faild, err: %v\n", err)
-	}
-
+	// 启动
+	r.Run()
 }
